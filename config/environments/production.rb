8783130f229f1473f6a82de8351c35cd1697deda
sujaysuzzi <sujaysudheendra@gmail.com>
2016-11-05 17:17:05 -0500

    prepare for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,11 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_cable.allowed_request_origins = ['https://openchatapp.herokuapp.com/',
+                                               'http://openchatapp.herokuapp.com/']
+
+  config.action_cable.url = "wss://openchatapp.herokuapp.com//cable"
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
