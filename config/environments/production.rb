bb1439f99e9d11b99f1f4966bb7d46677b16f2e2
sujaysuzzi <sujaysudheendra@gmail.com>
2016-11-05 19:11:58 -0500

    action cable not working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,11 +14,11 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
+  config.action_cable.url = "http://openchatapp.herokuapp.com/cable"
+
   config.action_cable.allowed_request_origins = ['https://openchatapp.herokuapp.com/',
                                                'http://openchatapp.herokuapp.com/']
-
-  config.action_cable.url = "wss://openchatapp.herokuapp.com//cable"
-
+  #
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
@@ -34,8 +34,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
+  config.assets.compile = true
+  config.public_file_server.enabled = true
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
